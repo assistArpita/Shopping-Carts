04900f0fd95ade04dd1a22020a8f8a1b8f57649f
arjun rai <arjun rai@DESKTOP-221UMG0>
2024-06-06 15:56:57 +0530

    initial commit

---
+++ Shopping-cart/src/main/java/shoping/com/servlet/OrderNowServlet.java
@@ -0,0 +1,77 @@
+package shoping.com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.*;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import shoping.com.connection.DbCon;
+import shoping.com.dao.OrderDao;
+import shoping.com.model.Cart;
+import shoping.com.model.Order;
+import shoping.com.model.User;
+
+import java.text.SimpleDateFormat;
+
+
+
+@WebServlet("/order-now")
+public class OrderNowServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+            Date date = new Date();
+
+            User auth = (User) request.getSession().getAttribute("auth");
+
+            if (auth != null) {
+                String productId = request.getParameter("id");
+                int productQuantity = Integer.parseInt(request.getParameter("quantity"));
+                if (productQuantity <= 0) {
+                	productQuantity = 1;
+                }
+                Order orderModel = new Order();
+                orderModel.setId(Integer.parseInt(productId));
+                orderModel.setUid(auth.getId());
+                orderModel.setQunatity(productQuantity);
+                orderModel.setDate(formatter.format(date));
+
+                OrderDao orderDao = new OrderDao(DbCon.getConnection());
+                boolean result = orderDao.insertOrder(orderModel);
+                if (result) {
+                    ArrayList<Cart> cart_list = (ArrayList<Cart>) request.getSession().getAttribute("cart-list");
+                    if (cart_list != null) {
+                        for (Cart c : cart_list) {
+                            if (c.getId() == Integer.parseInt(productId)) {
+                                cart_list.remove(cart_list.indexOf(c));
+                                break;
+                            }
+                        }
+                    }
+                    response.sendRedirect("order.jsp");
+                } else {
+                    out.println("order failed");
+                }
+            } else {
+                response.sendRedirect("login.jsp");
+            }
+
+        } catch (ClassNotFoundException|SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+    
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ Shopping-cart/src/main/java/shoping/com/servlet/package-info.java
@@ -0,0 +1,1 @@
+package shoping.com.servlet;
